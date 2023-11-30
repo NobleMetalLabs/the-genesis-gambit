a9ab4d2236fb1da2707511571a8256f3dd5c8eee
20ml <milliliter.avc@gmail.com>
2023-11-30 14:07:31 -0500

    Improve card dragging

--- Card.gd
+++ Card.gd
@@ -3,6 +3,7 @@ extends Node
 
 @onready var area = $Area2D
 var dragging : bool = false
+var dragging_offset : Vector2 = Vector2.ZERO
 
 func _ready():
 	area.input_event.connect(
@@ -11,16 +12,18 @@ func _ready():
 			if not event.button_index == MOUSE_BUTTON_LEFT: return
 			if event.pressed: start_drag() 
 			else: end_drag()
+			get_viewport().set_input_as_handled()
 	)
 
 func _process(_delta):
 	if dragging:
-		self.position = get_parent().get_local_mouse_position()
+		self.position = get_parent().get_local_mouse_position() + dragging_offset
 		if not Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
 			end_drag()
 
 func start_drag():
 	dragging = true
+	dragging_offset = self.position - get_parent().get_local_mouse_position()
 	print("drag")
 
 func end_drag():

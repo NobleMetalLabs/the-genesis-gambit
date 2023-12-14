37443741e9212bed91ceabd76f0a660619991a2d
20ml <milliliter.avc@gmail.com>
2023-12-14 18:51:11 -0500

    Refine GDScript settings and fix existing scripts

--- src/CardInstance.gd
+++ src/CardInstance.gd
@@ -1,40 +1,36 @@
 class_name CardInstance
 extends Node
 
-@export var data : CardMetadata :
-	set(value): set_data(value)
+@export var data : CardMetadata
 
-@onready var area = $Area2D
-@onready var sprite = $Sprite2D
+@onready var area : Area2D = $Area2D
+@onready var sprite : Sprite2D = $Sprite2D
 var dragging : bool = false
 var dragging_offset : Vector2 = Vector2.ZERO
 
-func _ready():
+func _ready() -> void:
 	area.input_event.connect(
-		func (_viewport, event, _shape_idx):
+		func (_viewport : Viewport, event : InputEvent, _shape_idx : int) -> void:
 			if not event is InputEventMouseButton: return
 			if not event.button_index == MOUSE_BUTTON_LEFT: return
 			if event.pressed: start_drag() 
 			else: end_drag()
 			get_viewport().set_input_as_handled()
 	)
-
-func set_data(data : CardMetadata):
+	
 	sprite.texture = data.image
-	# rarity sprite
-	# set stats
 
-func _process(_delta):
+func _process(_delta : float) -> void:
 	if dragging:
 		self.position = get_parent().get_local_mouse_position() + dragging_offset
 		if not Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
 			end_drag()
 
-func start_drag():
+func start_drag() -> void:
 	dragging = true
 	dragging_offset = self.position - get_parent().get_local_mouse_position()
 	print("drag")
 
-func end_drag():
+func end_drag() -> void:
 	dragging = false
 	print("end drag")
--- tst/unit/test_GamefieldManager.gd
+++ tst/unit/test_GamefieldManager.gd
@@ -3,31 +3,33 @@ extends GutTest
 class TestGamefieldStateHandling:
 	extends GutTest
 
-	var gamefield_scn_res = preload("res://scn/Gamefield.tscn")
-	var gamefield = null
-	func before_each():
+	var gamefield_scn_res : PackedScene = preload("res://scn/Gamefield.tscn")
+	var gamefield : Node2D = null
+
+	func before_each() -> void:
 		gamefield = gamefield_scn_res.instantiate()
 		self.add_child(gamefield)
-	func after_each():
+
+	func after_each() -> void:
 		gamefield.free()
 	
-	func test_export_empty():
+	func test_export_empty() -> void:
 		pass
 
 class TestGamefieldCardManipulation:
 	extends GutTest
 
-	func test_place_card():
+	func test_place_card() -> void:
 		pass
 
 class TestGamefieldEventProcessing:
 	extends GutTest
 
-	func test_process_event_single():
+	func test_process_event_single() -> void:
 		pass
 
-	func test_process_event_multiple():
+	func test_process_event_multiple() -> void:
 		pass
 
-	func test_process_event_frozen():
+	func test_process_event_frozen() -> void:
 		pass

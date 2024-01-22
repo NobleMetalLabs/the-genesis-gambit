5c367790d7f387897e91e57bccc6fef415b3e9cc
20ml <milliliter.avc@gmail.com>
2024-01-22 16:38:58 -0500

    Add some identifier classes

---
+++ lib/identifiers/ICardInstance.gd
@@ -0,0 +1,7 @@
+class_name ICardInstance
+extends Identifier
+
+var metadata : CardMetadata
+
+func get_metadata() -> CardMetadata:
+	return metadata
---
+++ lib/identifiers/ITargetable.gd
@@ -0,0 +1,2 @@
+class_name ITargetable
+extends Identifier

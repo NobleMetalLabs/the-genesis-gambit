c75ecf277fb0d75bd9f1bb24967561624be97eb7
ethkog <gdethkog2@gmail.com>
2024-02-16 13:55:17 -0800

    Add UIPackSlice and SelectPacks functionality
    Everything here needs to be changed

--- src/menu/SelectPacks.gd
+++ src/menu/SelectPacks.gd
@@ -2,9 +2,11 @@ extends Node
 
 @export var cardpack_path : String = "res://ast/game/cardpacks/data"
 @export var pack_scene : PackedScene
+@export var pack_slice_scene : PackedScene
 @onready var pack_container : GridContainer = $"%GRID-CONTAINER"
 @onready var rarity_filter : OptionButton = $"%RARITY-FILTER"
 @onready var type_filter : OptionButton = $"%TYPE-FILTER"
+@onready var slice_container : VBoxContainer = $"%SLICE-CONTAINER"
 
 func _ready() -> void:
 	scan_packs(cardpack_path)
@@ -19,6 +21,11 @@ func scan_packs(path : String) -> void:
 		pack_container.add_child(new_pack, true)
 		new_pack.set_metadata(new_pack_data)
 		new_pack.custom_minimum_size = Vector2(135,200)
+		new_pack.full_button.pressed.connect(
+			func() -> void:
+				add_slice(new_pack_data)
+				SaveData.selected_decks[new_pack_data.rarity].append(new_pack_data)
+		)
 
 var selected_rarity : int = 0
 var selected_type : int = 0
@@ -30,3 +37,14 @@ func filter_packs(_index : int) -> void:
 		var rarity_good : bool = (selected_rarity == 0 or pack._metadata.rarity == rarity_filter.get_item_text(selected_rarity))
 		var type_good : bool = (selected_type == 0 or pack._metadata.type == type_filter.get_item_text(selected_type))
 		pack.visible = rarity_good and type_good
+
+func add_slice(_metadata : PackMetadata) -> void:
+	var selected_rarity_array : Array = SaveData.selected_decks[_metadata.rarity]
+	if selected_rarity_array.has(_metadata):
+		var count : int = selected_rarity_array.count(_metadata)
+		slice_container.get_node(_metadata.name).set_label_multiplier(count + 1)
+	else:
+		var new_pack_slice : UIPackSlice = pack_slice_scene.instantiate()
+		slice_container.add_child(new_pack_slice)
+		new_pack_slice.name = _metadata.name
+		new_pack_slice.set_metadata(_metadata)

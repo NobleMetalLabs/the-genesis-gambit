c4f5fdcb74d43a6cae48a2b083366ac0072ab75a
20ml <milliliter.avc@gmail.com>
2024-08-08 20:44:01 -0400

    Remove CBN code

--- lib/card/behavior/frontend/CardBehaviorGraph.gd
+++
@@ -1,7 +0,0 @@
-@icon("res://lib/cardbehavior/CardBehaviorGraph.png")
-class_name CardBehaviorGraph
-extends Serializeable
-
-var description : String = ""
-var nodes : Array[CardBehaviorNodeInstance] = []
-var edges : Array[CardBehaviorEdge] = []

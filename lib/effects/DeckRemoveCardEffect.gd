5d464ae620bae413706b17dbc30e1950cff4aca5
20ml <milliliter.avc@gmail.com>
2024-08-09 14:15:38 -0400

    Implement max hand size

--- lib/effects/DeckRemoveCardEffect.gd
+++ lib/effects/DeckRemoveCardEffect.gd
@@ -20,14 +20,14 @@ func resolve(_effect_resolver : EffectResolver) -> void:
 	IStatisticPossessor.id(self.card).set_statistic(Genesis.Statistic.IS_IN_DECK, false)
 	IStatisticPossessor.id(player).modify_statistic(Genesis.Statistic.NUM_CARDS_LEFT_IN_DECK, -1)
 
+	player.cards_in_deck.erase(card_instance)
+	
 	if leave_reason == Genesis.LeaveDeckReason.DRAWN:
-		#Card already removed from deck by DeckDrawCardEffect
 		_effect_resolver.request_effect(HandAddCardEffect.new(
 			self.requester, self.player, card,
 		))
 
 	if leave_reason == Genesis.LeaveDeckReason.PLAYED:
-		player.cards_in_deck.erase(card_instance)
 		_effect_resolver.request_effect(CreatureSpawnEffect.new(
 			self.requester, card_instance
 		))

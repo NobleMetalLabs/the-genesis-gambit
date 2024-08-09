dab51a9822f5e37e0a7f246762c828cb1f05419d
20ml <milliliter.avc@gmail.com>
2024-08-09 07:00:58 -0400

    Export build 0.1.0

--- src/autoloads/AudioDispatcher.gd
+++ src/autoloads/AudioDispatcher.gd
@@ -6,9 +6,9 @@ var sound_path : String = "res://ast/sound/"
 func dispatch_audio(audio : StringName) -> AudioStreamPlayer:
 	var new_player : AudioStreamPlayer = AudioStreamPlayer.new()
 	
-	if FileAccess.file_exists("%s/%s.tres" % [sound_path, audio]):
+	if FileAccess.file_exists("%s/%s.tres" % [sound_path, audio]) or FileAccess.file_exists("%s/%s.tres.import" % [sound_path, audio]):
 		new_player.stream = ResourceLoader.load("%s/%s.tres" % [sound_path, audio])
-	elif FileAccess.file_exists("%s/%s.wav" % [sound_path, audio]):
+	elif FileAccess.file_exists("%s/%s.wav" % [sound_path, audio]) or FileAccess.file_exists("%s/%s.wav.import" % [sound_path, audio]):
 		new_player.stream = ResourceLoader.load("%s/%s.wav" % [sound_path, audio])
 	else:
 		push_error("Audio file not found: %s" % audio)
@@ -23,9 +23,9 @@ func dispatch_audio(audio : StringName) -> AudioStreamPlayer:
 
 func dispatch_positional_audio(position : Vector2, audio : StringName) -> AudioStreamPlayer2D:
 	var new_player : AudioStreamPlayer2D = AudioStreamPlayer2D.new()
-	if FileAccess.file_exists("%s/%s.tres" % [sound_path, audio]):
+	if FileAccess.file_exists("%s/%s.tres" % [sound_path, audio]) or FileAccess.file_exists("%s/%s.tres.import" % [sound_path, audio]):
 		new_player.stream = ResourceLoader.load("%s/%s.tres" % [sound_path, audio])
-	elif FileAccess.file_exists("%s/%s.wav" % [sound_path, audio]):
+	elif FileAccess.file_exists("%s/%s.wav" % [sound_path, audio]) or FileAccess.file_exists("%s/%s.wav.import" % [sound_path, audio]):
 		new_player.stream = ResourceLoader.load("%s/%s.wav" % [sound_path, audio])
 	else:
 		push_error("Audio file not found: %s" % audio)

1328dffd1e10069830465e39ac18665d3bac573f
20ml <milliliter.avc@gmail.com>
2024-12-08 12:15:14 -0500

    Create foundational event processing architecture

---
+++ src/game/processing/EventCausalityLogger.gd
@@ -0,0 +1,46 @@
+class_name EventCausalityLogger
+extends RefCounted
+
+var event_causalities : Array[EventCausality] = []
+var event_to_causalities : Dictionary = {} # [Event, EventCausality]
+
+func register_event(event : Event) -> void:
+	var event_causality : EventCausality = _get_or_make_causality(event)
+	event_causality.event = event
+	self.event_causalities.append(event_causality)
+	self.event_to_causalities[event] = event_causality
+
+func register_caused_event(event : Event, caused_event : Event) -> void:
+	var event_causality : EventCausality = _get_or_make_causality(event)
+	var caused_event_causality : EventCausality = _get_or_make_causality(caused_event)
+	event_causality.add_caused_event(caused_event_causality)
+	caused_event_causality.add_caused_by_event(event_causality)
+
+func get_events_caused_by(event : Event) -> Array[Event]:
+	var event_causality : EventCausality = _get_or_make_causality(event)
+	var caused_events : Array[Event] = event_causality.caused_events.map(
+		func extract_event(caused_event_causality : EventCausality) -> Event:
+			return caused_event_causality.event
+	)
+	return caused_events
+
+func _get_or_make_causality(event : Event) -> EventCausality:
+	if self.event_to_causalities.has(event):
+		return self.event_to_causalities[event]
+	else:
+		var event_causality := EventCausality.new()
+		event_causality.event = event
+		self.event_causalities.append(event_causality)
+		self.event_to_causalities[event] = event_causality
+		return event_causality
+
+class EventCausality extends RefCounted:
+	var event : Event
+	var caused_events : Array[EventCausality]
+	var caused_by_events : Array[EventCausality]
+
+	func add_caused_event(event_causality : EventCausality) -> void:
+		caused_events.append(event_causality)
+
+	func add_caused_by_event(event_causality : EventCausality) -> void:
+		caused_by_events.append(event_causality)
---
+++ src/game/processing/EventScheduler.gd
@@ -0,0 +1,23 @@
+class_name EventScheduler
+extends Node
+
+
+var processing_steps_by_event_by_target : Dictionary = {} #[ICardInstance, Dictionary[StringName, Array[EventProcessingStep]]]
+var processing_step_by_requester : Dictionary = {} #[ICardInstance, Array[EventProcessingStep]]
+
+func register_event_processing_step(event_processing_step : EventProcessingStep) -> void:
+	var target_event_processing_steps : Dictionary = processing_steps_by_event_by_target.get(event_processing_step.target, {})
+	target_event_processing_steps.get_or_add(event_processing_step.event_type, []).append(event_processing_step)
+	processing_step_by_requester.get_or_add(event_processing_step.processing_source, []).append(event_processing_step)
+
+# Requests targeting unknowns or groups
+#func register_event_processing_step(target_schema : TargetSchema, event_type, function : Callable) -> void:
+
+func process_event(event : Event) -> void:
+	var processing_steps : Array[EventProcessingStep] = processing_steps_by_event_by_target.get(event.target, {}).get(event.event_type, [])
+	processing_steps.sort_custom(func sort_by_priority_int_descending(a : EventProcessingStep, b : EventProcessingStep) -> bool:
+		return a.priority.to_int() > b.priority.to_int()
+	)
+	for processing_step in processing_steps:
+		processing_step.function.call(event)
+	

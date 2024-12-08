8fe546f937d497eb26c90b8aa0a8d7487709eb86
20ml <milliliter.avc@gmail.com>
2024-12-07 20:42:49 -0500

    Fix slug

--- ast/game/cards/logic/src/bugs/Slug.gd
+++ ast/game/cards/logic/src/bugs/Slug.gd
@@ -7,11 +7,11 @@ var last_targeted_creature : ICardInstance
 func HANDLE_TARGETED(event : TargetedEvent) -> void:
 	if last_targeted_creature != null:
 		last_targeted_creature.logic.WAS_ATTACKED.disconnect(HANDLE_WAS_ATTACKED_ON_TARGET)
-	last_targeted_creature = event.target
+	last_targeted_creature = event.who
 	last_targeted_creature.logic.WAS_ATTACKED.connect(HANDLE_WAS_ATTACKED_ON_TARGET)
 	super(event)
 
 func HANDLE_WAS_ATTACKED_ON_TARGET(event : WasAttackedEvent) -> void:
 	game_access.card_processor.request_event(
-		GaveMoodEvent.new(event.by_who, owner, StatisticMood.SLOW(owner))
+		GaveMoodEvent.new(owner, event.by_who, StatisticMood.SLOW(owner))
 	)

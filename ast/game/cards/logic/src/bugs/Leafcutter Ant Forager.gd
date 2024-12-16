114b48d5a65fe9a3fb73fdd17058358a5027d181
ethkog <gdethkog2@gmail.com>
2024-12-16 02:53:03 -0800

    a few cards

---
+++ ast/game/cards/logic/src/bugs/Leafcutter Ant Forager.gd
@@ -0,0 +1,37 @@
+extends CardLogic
+
+static var description : StringName = "On kill: Friendly Fungus Gardens gain 1 charge."
+
+var fungus_gardens : Array[ICardInstance] = []
+
+func _register_processing_steps() -> void:
+	game_access.event_scheduler.register_event_processing_step(
+		EventProcessingStep.new(AllCardsTargetGroup.new(), "ENTERED_FIELD", owner, WATCH_FUNGUS_GARDENS, 
+			EventPriority.new().STAGE(EventPriority.PROCESSING_STAGE.POSTEVENT).RARITY_FROM_CARD(owner)
+	))
+	game_access.event_scheduler.register_event_processing_step(
+	EventProcessingStep.new(SingleCardTargetGroup.new(owner), "KILLED", owner, GIVE_CHARGE_TO_GARDENS, 
+		EventPriority.new().STAGE(EventPriority.PROCESSING_STAGE.POSTEVENT).RARITY_FROM_CARD(owner)
+	))
+
+func WATCH_FUNGUS_GARDENS(event: EnteredFieldEvent) -> void:
+	if event.card.metadata.id != CardDB.get_id_by_name("fungus-garden"): return
+	if not game_access.are_two_cards_friendly(owner, event.card): return
+	fungus_gardens.append(event.card)
+	
+	game_access.event_scheduler.register_event_processing_step(
+	EventProcessingStep.new(SingleCardTargetGroup.new(event.card), "LEFT_FIELD", owner, DEWATCH_FUNGUS_GARDEN, 
+		EventPriority.new().STAGE(EventPriority.PROCESSING_STAGE.POSTEVENT).RARITY_FROM_CARD(owner)
+	))
+
+func GIVE_CHARGE_TO_GARDENS(event: KilledEvent) -> void: 
+	for fungus_garden : ICardInstance in fungus_gardens:
+		var charges : int = IStatisticPossessor.id(fungus_garden).get_statistic(Genesis.Statistic.CHARGES)
+		game_access.card_processor.request_event(
+			SetStatisticEvent.new(fungus_garden, Genesis.Statistic.CHARGES, charges + 1)
+		)
+
+func DEWATCH_FUNGUS_GARDEN(event: LeftFieldEvent) -> void:
+	fungus_gardens.erase(event.card)
+
+# NOTE: create ModifyStatisticEvent that just invokes a SetStatisticEvent with the correct value?
---
+++ ast/game/cards/logic/src/bugs/Pheremones.gd
@@ -0,0 +1,38 @@
+extends CardLogic
+
+static var description : StringName = "Each friendly Damage card gains Boredom."
+
+var friendly_damage : Array[ICardInstance] = []
+
+func _register_processing_steps() -> void:
+	game_access.event_scheduler.register_event_processing_step(
+		EventProcessingStep.new(AllCardsTargetGroup.new(), "ENTERED_FIELD", owner, WATCH_DAMAGE, 
+			EventPriority.new().STAGE(EventPriority.PROCESSING_STAGE.POSTEVENT).RARITY_FROM_CARD(owner)
+	))
+	game_access.event_scheduler.register_event_processing_step(
+		EventProcessingStep.new(SingleCardTargetGroup.new(owner), "ENTERED_FIELD", owner, DAMAGE_GETS_BORED, 
+			EventPriority.new().STAGE(EventPriority.PROCESSING_STAGE.POSTEVENT).RARITY_FROM_CARD(owner)
+	))
+
+func WATCH_DAMAGE(event: EnteredFieldEvent) -> void:
+	if event.card.metadata.type != Genesis.CardType.ATTACKER: return # NOTE: lol
+	if not game_access.are_two_cards_friendly(owner, event.card): return
+	friendly_damage.append(event.card)
+	
+	game_access.event_scheduler.register_event_processing_step(
+		EventProcessingStep.new(SingleCardTargetGroup.new(event.card), "LEFT_FIELD", owner, DEWATCH_DAMAGE, 
+			EventPriority.new().STAGE(EventPriority.PROCESSING_STAGE.POSTEVENT).RARITY_FROM_CARD(owner)
+	))
+
+func DEWATCH_DAMAGE(event: LeftFieldEvent) -> void:
+	friendly_damage.erase(event.card)
+
+func DAMAGE_GETS_BORED(_event: EnteredFieldEvent) -> void:
+	for damage : ICardInstance in friendly_damage:
+		game_access.card_processor.request_event(
+			GaveMoodEvent.new(owner, damage, BoredomMood.new(owner))
+		)
+	
+	game_access.card_processor.request_event(
+		KilledEvent.new(owner, owner)
+	)
